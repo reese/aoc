3e327f8382c0dfb0a1c41137ca0e67e9321f31c3
Reese Williams <reese@reesew.io>
2023-11-30 20:19:18 +0000

    Initial setup

---
+++ src/day_1/mod.rs
@@ -0,0 +1,10 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+#[aoc_generator(day1)]
+fn input_generator(_input: &str) -> i32 {
+    unimplemented!()
+}
+
+#[aoc(day1, part1)]
+fn part1(_freqs: &i32) -> i32 {
+    unimplemented!()
+}
